32e9c14ce71715053513d07429a436cc610df27d
DiCesareDaniele <dicesaredaniele7@gmail.com>
2026-01-03 19:10:18 +0100

    tmux improved

--- modules/home/cli-apps/tmux/scripts/open-github.sh
+++ modules/home/cli-apps/tmux/scripts/open-github.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 
-cd $(tmux run "echo #{pane_start_path}")
-url=$(git remote get-url origin) 
+PATHS=(
+  $(tmux run "echo #{pane_current_path}")
+  $(tmux run "echo #{pane_start_path}")
+)
 
-if [[ $url == *github.com* ]]; then
+for path in "${PATHS[@]}"; do
+  cd "$path"
+  url=$(git remote get-url origin)
+  if [[ $url == *github.com* ]]; then
     if [[ $url == git@* ]]; then
-        url="${url#git@}"
-        url="${url/:/\/}" 
-        url="https://$url"
+      url="${url#git@}"
+      url="${url/:/\/}"
+      url="https://$url"
     fi
-    open "$url"
-else
-    echo "This repository is not hosted on GitHub"
-fi
+    xdg-open "$url"
+    exit 0
+  fi
+done
+
+echo "This repository is not hosted on GitHub"
 
--- modules/home/cli-apps/tmux/scripts/tmux-session-dispensary.sh
+++ modules/home/cli-apps/tmux/scripts/tmux-session-dispensary.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 DIRS=(
-    "$HOME"
+  "$HOME"
 )
 
 if [[ $# -eq 1 ]]; then
-    selected=$1
+  selected=$1
 else
-    selected=$(find . "${DIRS[@]}" -type d -mindepth 1 -maxdepth 1 \
-        | sed "s|^$HOME/||" \
-        | fzf --margin 10% --color="bw")
+  selected=$(find . "${DIRS[@]}" -type d -mindepth 1 -maxdepth 1 \
+    | sed "s|^$HOME/||" \
+    | fzf --margin 10% --color="bw")
 
-    [[ $selected ]] && selected="$HOME/$selected"
+  [[ $selected ]] && selected="$HOME/$selected"
 fi
 
 [[ ! $selected ]] && exit 0
@@ -19,8 +19,8 @@ fi
 selected_name=$(basename "$selected" | tr . _)
 
 if ! tmux has-session -t "$selected_name"; then
-    tmux new-session -ds "$selected_name" -c "$selected"
-    tmux select-window -t "$selected_name:1"
+  tmux new-session -ds "$selected_name" -c "$selected"
+  tmux select-window -t "$selected_name:1"
 fi
 
 tmux switch-client -t "$selected_name"
