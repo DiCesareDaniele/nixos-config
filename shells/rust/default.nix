3e5a5c19fe1d17bd04a3d5828b00b2cb59f38aa3
DiCesareDaniele <dicesaredaniele7@gmail.com>
2026-06-02 22:29:46 +0200

    flake update

--- shells/rust/default.nix
+++ shells/rust/default.nix
@@ -1,21 +1,20 @@
-{ pkgs, ... }:
-with pkgs;
-mkShell rec {
-  nativeBuildInputs = [
-    pkg-config
-  ];
+{ ... }:
+let
+  rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+  rustVersion = "latest";
+  rust = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+    extensions = [
+      "rust-src"
+      "rust-analyzer"
+    ];
+  });
+in
+pkgs.mkShell {
   buildInputs = [
-    openssl
-    cargo
-    rustc
-    rust-analyzer
-    rustfmt
-    clippy
-  ];
-  RUST_SRC_PATH = "${rust.packages.stable.rustPlatform.rustLibSrc}";
-  # NIX_LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
-  # NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";
-  # shellHook = ''
-  #   export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
-  # '';
+    rust
+  ] ++ (with pkgs; [
+    pkg-config
+  ]);
+  RUST_BACKTRACE = 1;
 }
